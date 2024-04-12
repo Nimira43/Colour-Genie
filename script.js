017fbcce85faa86b4ddeafc8b206e3d9e56e7d1e
Nick Rabone <n.rabone72@aol.com>
2024-04-12 15:27:42 +0100

    Tidy up

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const containerEl = document.querySelector('.container')
 
-for (let index = 0; index < 30; index++) {
+for (let i = 0; i < 30; i++) {
   const colourContainerEl = document.createElement('div')
   colourContainerEl.classList.add('colour-container')
   containerEl.appendChild(colourContainerEl)
 }
-
-
 
 const generateColours = () => {
   colourContainerEls.forEach((colourContainerEl) => {
@@ -20,7 +18,7 @@ const randomColour = () => {
   const chars = '0123456789abcdef'
   const colourCodeLength = 6
   let colourCode = ''
-  for (let index = 0; index < colourCodeLength; index++) {
+  for (let i = 0; i < colourCodeLength; i++) {
     const randomNum = Math.floor(Math.random() * chars.length)
     colourCode += chars.substring(randomNum, randomNum + 1)
   }
